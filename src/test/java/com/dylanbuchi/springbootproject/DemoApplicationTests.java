e4e261dfec409ae7c12345605ff8b3afbea5e9f0
Dylan <devybuchi@gmail.com>
2020-01-27 06:41:29 -0300

    ''

---
+++ src/test/java/com/dylanbuchi/springbootproject/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dylanbuchi.springbootproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
